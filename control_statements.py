a7459e2dbf872cc41181823b00226027ccdfc37a
babithdarshan <sathishbabith1990@gmail.com>
2021-05-25 23:43:38 +0530

    python control structures

---
+++ control_statements.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+name=input('enter your name:')
+if name == 'sathish':
+    print('hello sathish gud mrng')
+print('how are you')
+
+
+# In[5]:
+
+
+name=input('enter your name:')
+if name== 'sathish':
+    print('hello sathish you look handosme')
+print('how are you')
+
+
+# In[6]:
+
+
+name=input('enter your name:')
+if name == 'sathish':
+    print('hello sathish nice meeting you')
+else:
+    print('hello guest your not a valid user')
+print('how are you')
+
+
+# In[16]:
+
+
+brand = input('enter your favourite brand:')
+if brand =='rc':
+    print('its childrens brand')
+elif brand == 'ke':
+    print('its not that much kick')
+elif brand == 'kf':
+    print('its is too light')
+elif brand == 'foster':
+    print('by one get one')
+else:
+    print('other brands are not recomended')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
