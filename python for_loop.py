12a0729486cb6d487ed311679859b4f0ebc5e408
babithdarshan <sathishbabith1990@gmail.com>
2021-05-27 19:36:14 +0530

    for loop

---
+++ python for_loop.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+s=input('enter any string:')
+i=0
+for ch in s:
+    
+     print('the character present at',i,'index:',ch)
+     i=i+1
+
+
+# In[11]:
+
+
+for x in range(1,13):
+    print('hello')
+
+
+# In[14]:
+
+
+for x in range(0,22,2):
+    print(x)
+
+
+# In[15]:
+
+
+for x in range(21):
+     if x%2 !=0:
+            print(x)
+        
+
+
+# In[17]:
+
+
+for x in range(10,0,-1):
+    print(x)
+
+
+# In[36]:
+
+
+l=eval(input('Enter List:'))
+sum=0
+for x in l:
+    sum=sum+x
+print('the sum:',sum)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
