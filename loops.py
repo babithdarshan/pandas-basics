d92e0cb705351be76d7c5b7138715b8879aec9dc
babithdarshan <sathishbabith1990@gmail.com>
2021-05-29 23:43:35 +0530

    if-elif-else

---
+++ loops.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+n1=int(input('enter first number:'))
+n2=int(input('enter second number:'))
+n3=int(input('enter third number:'))
+if n1<n2  and n2<n3:
+    print('n1 is smaller')
+elif n2<n3:
+    print('n2 is smaller')
+else:
+    print('n3 is bigger')
+
+
+# In[9]:
+
+
+number=int(input('enter a number:'))
+if n%2 == 0:
+    print('{0} is even number'.format(number))
+elif n%3 ==0:
+        print('{0} is odd number'.format(number))
+
+
+# In[24]:
+
+
+n=int(input('enter n number:'))
+if n ==0:
+    print('zero')
+elif n ==1:
+    print('one')
+elif n==2:
+    print('two')
+elif n==3:
+    print('three')
+elif n==4:
+    print('four')
+elif n==5:
+    print('five')
+elif n==6:
+    print('six')
+else:
+    print('numbers from 0 to 9 only')
+    
+        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
