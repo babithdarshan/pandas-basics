ee3fe7a860e371e9fc48ccf7765b2dfea4f7f179
babithdarshan <sathishbabith1990@gmail.com>
2021-06-30 22:48:16 +0530

    string methods

---
+++ string methods.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print('sathish123'.isalnum())
+
+
+# In[5]:
+
+
+print('sathish'.isalpha())
+print('sathish'.isalnum)
+
+
+# In[6]:
+
+
+print('1234'.isdigit())
+
+
+# In[7]:
+
+
+print('sathish'.islower())
+
+
+# In[10]:
+
+
+print('SATHISH'.isupper())
+
+
+# In[11]:
+
+
+print('Sathish Darshanala'.istitle())
+
+
+# In[14]:
+
+
+print('sathish darshanala'.istitle())
+print(''.isspace())
+
+
+# In[23]:
+
+
+s='learning python is very easy'
+print(s.startswith('learning'))
+
+
+# In[24]:
+
+
+print(s.endswith('easy'))
+
+
+# In[25]:
+
+
+s= 'the cost of the product is $ 123.43 per piece.'
+l=s.split()
+
+
+# In[26]:
+
+
+print(l)
+
+
+# In[27]:
+
+
+print(l[6],l[7])
+
+
+# In[ ]:
+
+
+
+
